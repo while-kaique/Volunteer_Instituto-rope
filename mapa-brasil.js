f73d65cfc27db13e45be71863d4548d21190b2ed
while-kaique <123902283+while-kaique@users.noreply.github.com>
2023-10-17 01:33:58 -0300

    Criei mapa interativo - cada estado
    Fiz a configuração inicial, em breve melhorias no design

---
+++ mapa-brasil.js
@@ -0,0 +1,31 @@
+
+const description = document.querySelector(".tooltip-mapa");
+    
+document.querySelectorAll('path').forEach((el) =>{
+
+  const logo = '<img src="/images/webdoor-logo.png" alt="logo-png" style="height: 40px">'
+  el.addEventListener('mouseover', (event) => {
+    event.target.className = ("enabled");
+    description.classList.add("active");
+    if ( el.id == "Minas Gerais" || el.id == "Bahia" || el.id == "Amapá" || el.id == "Pernambuco" || el.id == "São Paulo") {
+      description.classList.add("logo");
+      description.innerHTML = 'Clique e veja mais sobre nossas filiais'
+    } else {
+      description.innerHTML = event.target.id; 1 
+    }
+    
+  })
+  }
+)
+
+document.querySelectorAll('path').forEach((el) =>
+  el.addEventListener("mouseout", () => {
+    description.classList.remove("active");
+    description.classList.remove("logo");
+  })
+);
+
+document.onmousemove = function (e) {
+  description.style.left = e.pageX + "px";
+  description.style.top = (e.pageY - 90) + "px";
+}
